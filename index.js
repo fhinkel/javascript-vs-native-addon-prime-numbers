fe855f060965b8a1ee1a99d0202e24e3282d5d6b
Franziska Hinkelmann <franzih@chromium.org>
2017-12-04 18:09:06 +0100

    Modify code to time very small prime numbers

--- index.js
+++ index.js
@@ -17,52 +17,56 @@ function run(i) {
     console.log(i);
 
     console.time('Prime in js');
-    js.prime(i)
+    let upper = 1000000;
+    for(let j = 0; j < upper; j++) {
+        js.prime(i);
+    }
     console.timeEnd('Prime in js');
 
     console.time('Prime in addon');
-    addon.prime(i)
+    for(let j = 0; j < upper; j++) {        
+        addon.prime(i);
+    }
     console.timeEnd('Prime in addon');
 }
 
-// Compute lots of primes.
-for(let i = 1; i < 200; i++) {
-    run(i);
-}
-
-for(let i = 2; i < 10; i++) {
-    run(i*100);
-}
-
-for(let i = 1; i < 10; i++) {
-    run(i*1000);
-}
-
-for(let i = 1; i < 10; i++) {
-    run(i*10000);
-}
-
-for(let i = 1; i < 10; i++) {
-    run(i*100000);
-}
-
-run(1000000);
-
-// Sanity checks.
+// Warm up.
 const checks = [ 
     [1,2],
     [7,17],
     [10000, 104729],
     [100000, 1299709],
-    [1000000, 15485863],
-    [10000000, 179424673]
+    // [1000000, 15485863],
+    // [10000000, 179424673]
 ];
 
-// Run the checks at the end, otherwise
-// they turn on JS optimizations before we start running. 
 checks.forEach(function (a) {
     sanityCheck(...a); 
 });
+
+// Compute lots of primes.
+for(let i = 1; i < 60; i++) {
+    run(i);
+}
+
+// for(let i = 2; i < 10; i++) {
+//     run(i*100);
+// }
+
+// for(let i = 1; i < 10; i++) {
+//     run(i*1000);
+// }
+
+// for(let i = 1; i < 10; i++) {
+//     run(i*10000);
+// }
+
+// for(let i = 1; i < 10; i++) {
+//     run(i*100000);
+// }
+
+// run(1000000);
+
 
 console.log();
 console.log('So long and thanks for the fish.');
