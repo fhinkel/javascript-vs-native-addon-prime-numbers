6a37309291c5c1f1cfb7790c18c24fea9d088954
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2017-12-06 22:44:08 +0100

    Plot my data

--- public/chart.js
+++ public/chart.js
@@ -4,37 +4,36 @@ width = svg.attr("width") - margin.left - margin.right,
 height = svg.attr("height") - margin.top - margin.bottom,
 g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var parseTime = d3.timeParse("%Y%m%d");
-
-var x = d3.scaleTime().range([0, width]),
+var x = d3.scaleLinear().range([0, width]),
 y = d3.scaleLinear().range([height, 0]),
 z = d3.scaleOrdinal(d3.schemeCategory10);
 
 var line = d3.line()
 .curve(d3.curveBasis)
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.temperature); });
+.x(function(d) { return x(d.id); })
+.y(function(d) { return y(d.timing); });
 
-d3.tsv("data.tsv", type, function(error, data) {
+d3.tsv("wasm.tsv", type, function(error, data) {
 if (error) throw error;
 
-var cities = data.columns.slice(1).map(function(id) {
+var languages = data.columns.slice(1).map(function(id) {
 return {
   id: id,
   values: data.map(function(d) {
-    return {date: d.date, temperature: d[id]};
+    return {id: d.id, timing: d[id]*1000};
   })
 };
 });
 
-x.domain(d3.extent(data, function(d) { return d.date; }));
+console.log(d3.extent(data, function(d) { return +d.id; }));
+x.domain(d3.extent(data, function(d) { return +d.id; }));
 
 y.domain([
-d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
-d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
+d3.min(languages, function(c) { return d3.min(c.values, function(d) { return d.timing; }); }),
+d3.max(languages, function(c) { return d3.max(c.values, function(d) { return d.timing; }); })
 ]);
 
-z.domain(cities.map(function(c) { return c.id; }));
+z.domain(languages.map(function(c) { return c.id; }));
 
 g.append("g")
   .attr("class", "axis axis--x")
@@ -49,21 +48,21 @@ g.append("g")
   .attr("y", 6)
   .attr("dy", "0.71em")
   .attr("fill", "#000")
-  .text("Temperature, ºF");
+  .text("Milliseconds");
 
-var city = g.selectAll(".city")
-.data(cities)
+var language = g.selectAll(".language")
+.data(languages)
 .enter().append("g")
-  .attr("class", "city");
+  .attr("class", "language");
 
-city.append("path")
+language.append("path")
   .attr("class", "line")
   .attr("d", function(d) { return line(d.values); })
   .style("stroke", function(d) { return z(d.id); });
 
-city.append("text")
+language.append("text")
   .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-  .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+  .attr("transform", function(d) { return "translate(" + x(d.value.id) + "," + y(d.value.timing) + ")"; })
   .attr("x", 3)
   .attr("dy", "0.35em")
   .style("font", "10px sans-serif")
@@ -71,7 +70,7 @@ city.append("text")
 });
 
 function type(d, _, columns) {
-d.date = parseTime(d.date);
+d.id = +d.id;
 for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
 return d;
 }
