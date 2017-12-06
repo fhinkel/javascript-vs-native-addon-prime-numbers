fae05157ccd976fd822751fb89761e843b36c4c2
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2017-12-06 20:52:07 +0100

    Use express

--- index.js
+++ index.js
@@ -80,3 +80,11 @@ wasm.onRuntimeInitialized = () => {
     console.log();
     console.log('So long and thanks for the fish.');
 }
+
+ const express = require('express');
+
+ const app = express()
+ 
+ app.use(express.static('public'));
+ 
+ app.listen(3000, () => console.log('Example app listening on port 3000'))
